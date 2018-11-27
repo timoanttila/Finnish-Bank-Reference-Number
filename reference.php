834fa0ef9928a1b30a9051136ae2d5d942496732
Timo Anttila <tewdin@users.noreply.github.com>
2018-11-27 20:17:39 +0200

    Update and rename verification.php to reference.php

--- reference.php
+++ reference.php
@@ -4,11 +4,12 @@
 // Official numbers
 $numbers = array(1,3,7);
 
-// Reference numbers from $ref
-$ref_split = str_split($ref);
-
-// Some default values
+// Default values
 $sum = $i = 0;
+$ref = 3245;
+
+// Numbers from $ref
+$ref_split = str_split($ref);
 
 // Foreach all numbers from reference
 foreach($ref_split as $item){
@@ -16,6 +17,5 @@ foreach($ref_split as $item){
     if($i != 2) $i++; else $i = 0;
 }
 
-// Get nearest 10 and then that number minus $sum is your reference verification number which is the last number.
-$next = round($sum, -1);
-$ref = $ref . $next - $sum;
+// Round up to nearest 10. The new number minus $sum is the reference verification number.
+$ref = $ref . $sum = ceil($sum / 10) * 10 - $sum;
